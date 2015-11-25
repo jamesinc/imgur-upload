24f6df55499a6ecc98eee7d5e0d28a0471fdd4c9
James Ducker <james.ducker@ricewarner.com>
2015-11-26 10:01:53 +1100

    Initial commit. Dumped from forum repo. Added GPL licenses.

---
+++ class.imgurupload.plugin.php
@@ -0,0 +1,125 @@
+<?php if (!defined('APPLICATION')) exit();
+
+/**
+ * ImgurUpload Plugin
+ *
+ * Adds an image upload feature (with drag and drop!) that utilises the Imgur API
+ *
+ * @author James Ducker <james.ducker@gmail.com>
+ * @license http://opensource.org/licenses/gpl-3.0.html GPL
+ * @package Addons
+ */
+
+// Define the plugin:
+$PluginInfo['ImgurUpload'] = array(
+	'Description' => 'Adds an image upload feature (with drag and drop!) that utilises the Imgur API',
+	'Version' => '1.0.0',
+	'RequiredApplications' => array('Vanilla' => '2.1'),
+	'RequiredTheme' => FALSE,
+	'RequiredPlugins' => FALSE,
+	'HasLocale' => FALSE,
+	'MobileFriendly' => TRUE,
+	'SettingsUrl' => 'settings/imgurupload',
+	'SettingsPermission' => 'Garden.Settings.Manage',
+	'Author' => "James Ducker",
+	'AuthorEmail' => 'james.ducker@gmail.com',
+	'AuthorUrl' => 'https://github.com/jamesinc',
+	'License' => 'GPL-3.0'
+);
+
+class ImgurUploadPlugin extends Gdn_Plugin {
+
+	public function Setup() {}
+
+	public function SettingsController_ImgurUpload_Create($Sender) {
+		$Sender->Permission('Garden.Plugins.Manage');
+		$Sender->AddSideMenu('dashboard/settings/plugins');
+		$Sender->Title('ImgurUpload');
+		$ConfigurationModule = new ConfigurationModule($Sender);
+		$ConfigurationModule->RenderAll = True;
+		$Schema = array('Plugins.ImgurUpload.ClientID' => array(
+								'LabelCode' => 'Imgur API Client ID', 
+								'Control' => 'TextBox', 
+								'Default' => C('Plugins.ImgurUpload.ClientID', ''),
+								'Description' => 'Register for Imgur API access at: <a href="https://api.imgur.com/oauth2/addclient">https://api.imgur.com/oauth2/addclient</a>'
+							),
+						'Plugins.ImgurUpload.ProcessImageURLs' => array(
+								'LabelCode' => 'Process image URLs',
+								'Control' => 'Checkbox',
+								'Default' => C('Plugins.ImgurUpload.ProcessImageURLs', ''),
+								'Description' => 'Check the below checkbox to have the plugin attempt to identify when the user is dragging in an image URL.
+												This is usually if the user is dragging an image across from another web page.
+												If an image URL is detected, it will be wrapped with image markup.'
+							)
+
+		);
+		$ConfigurationModule->Schema($Schema);
+		$ConfigurationModule->Initialize();
+		$Sender->View = dirname(__FILE__) . DS . 'views' . DS . 'settings.php';
+		$Sender->ConfigurationModule = $ConfigurationModule;
+		$Sender->Render();
+	}
+
+	/**
+	 * DiscussionController_Render_Before HOOK
+	 *
+	 * Calls ImgurUpload::PrepareController
+	 *
+	 * @access public
+	 * @param mixed $Sender The hooked controller
+	 * @see ImgurUpload::PrepareController
+	 * @return void
+	 */
+	public function DiscussionController_Render_Before($Sender) {
+		$this->PrepareController($Sender);
+	}
+
+	/**
+	 * ConversationController_Render_Before HOOK
+	 *
+	 * Calls ImgurUpload::PrepareController
+	 *
+	 * @access public
+	 * @param mixed $Sender The hooked controller
+	 * @see ImgurUpload::PrepareController
+	 * @return void
+	 */
+	public function MessagesController_Render_Before($Sender) {
+		$this->PrepareController($Sender);
+	}
+
+	/**
+	 * PostController_Render_Before HOOK
+	 *
+	 * Calls ImgurUpload::PrepareController
+	 *
+	 * @access public
+	 * @param mixed $Sender The hooked controller
+	 * @see ImgurUpload::PrepareController
+	 * @return void
+	 */
+	public function PostController_Render_Before($Sender) {
+		$this->PrepareController($Sender);
+	}
+
+	/**
+	 * PrepareController function.
+	 *
+	 * Adds CSS and JS includes to the header of the discussion or post.
+	 *
+	 * @access protected
+	 * @param mixed $Controller The hooked controller
+	 * @return void
+	 */
+	protected function PrepareController($Controller) {
+		$ImgurClientID = C('Plugins.ImgurUpload.ClientID', '');
+		$ProcessImageURLs = C('Plugins.ImgurUpload.ProcessImageURLs', '');
+
+		// This becomes accessible in JS as gdn.definition("imgurclientid");
+		$Controller->AddDefinition('imgurclientid', $ImgurClientID);
+		$Controller->AddDefinition('processimageurls', $ProcessImageURLs);
+		$Controller->AddJsFile('dropzone.min.js', 'plugins/ImgurUpload');
+		$Controller->AddJsFile('imgurupload.min.js', 'plugins/ImgurUpload');
+		$Controller->AddCssFile('imgurupload.css', 'plugins/ImgurUpload');
+	}
+}
---
+++ js/imgurupload.js
@@ -0,0 +1,190 @@
+/*global gdn, jQuery */
+/**
+ * ImgurUpload, a drag'n'drop image upload tool for Vanilla Forums
+ * Copyright (C) 2015  James Ducker <james.ducker@gmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+(function ( $ ) {
+
+	"use strict";
+
+	// Inserts the given text at the caret position
+	// in the given input or textarea el.
+	var insertAtCursor = function ( el, text ) {
+
+		var sel, startPos, endPos;
+
+		text = text + '\n';
+
+		//IE support
+		if ( document.selection ) {
+
+			el.focus();
+			sel = document.selection.createRange();
+			sel.text = text;
+
+		} else if ( el.selectionStart || el.selectionStart == '0' ) {
+
+			startPos = el.selectionStart;
+			endPos = el.selectionEnd;
+			el.value = el.value.substring(0, startPos) + text +
+				el.value.substring(endPos, el.value.length);
+
+		} else {
+
+			el.value += text;
+
+		}
+
+	};
+
+	// Returns images in appropriate markup, depending on 
+	// forum settings.
+	var getLinkCode = function ( data ) {
+
+		var response,
+			type = gdn.definition( "InputFormat" );
+
+		switch ( type.toLowerCase() ) {
+			case "bbcode" :
+				response = '[img]' + data.link + '[/img]';
+				break;
+
+			case "markdown" :
+				response = '![](' + data.link + ')';
+				break;
+
+			case "html" :
+				// Specify width and height, so your users don't get annoyed with the page moving around as images load!
+				response = '<img src="' + data.link + '" alt="" width="' + data.width + '" height="' + data.height + '" />';
+				break;
+
+			default :
+				response = data.link;
+				break;
+
+		}
+
+		return response;
+
+	};
+
+	var getDropzoneConfig = function ( ta, previewCtx, clickable ) {
+		return {
+			success: function ( file, response ) {
+				if ( response.success ) {
+					insertAtCursor( ta[0], getLinkCode(response.data) );
+				} else {
+					// ultra-basic error handling
+					alert( "Something went wrong trying to upload your images :( \n\nOur image host, imgur.com, may be having technical issues. Give it a few minutes and try again." );
+				}
+			},
+			// Accept all image types
+			acceptedFiles: "image/*",
+			paramName: "image",
+			clickable: clickable,
+			method: "post",
+			maxFilesize: 20,
+			maxFiles: 20,
+			previewsContainer: previewCtx[0],
+			thumbnailWidth: 60,
+			thumbnailHeight: 60,
+			fallback: function ( ) {
+				
+			},
+			url: "https://api.imgur.com/3/upload",
+			headers: {
+				Authorization: "Client-ID " + gdn.definition("imgurclientid"),
+				Accept: "application/json"
+			}
+		};
+
+	};
+
+	$(function ( ) {
+
+			// In discussions, conversations, etc, the main input is always #Form_Body,
+			// but all the other IDs change, so we'll find everything relative to this element.
+		var fileInput,
+			ta = $( "#Form_Body" ),
+			form = ta.parents( "form" ),
+			submitBtn = form.find( "[type=submit]" ),
+			previewCtx = $("<div/>", {
+				"class": "imguruploader-preview-ctx"
+			});
+
+			// Be lazy like me and just tell your users the good news, rather than
+			// trying to modify the UI to make it obvious that drag'n'drop is supported.
+			ta.attr( "placeholder", "You can now drag and drop images here to add them to your post! They will appear wherever your caret is." );
+			ta.after( previewCtx );
+			
+			// Setup the dropzone
+			ta.dropzone( getDropzoneConfig(ta, previewCtx, false) );
+
+			// If we are dealing with a touch-screen device, or a device that reports to be a touch-screen device,
+			// we should show a button also, as most touch-screen devices are mobiles, which don't support file drag'n'drop.
+			// This method isn't perfect but it's pretty effective.
+			if ( 'ontouchstart' in window ) {
+
+				ta.attr( "placeholder", "" );
+				fileInput = $( "<a href=\"#\" class=\"Button ButtonAddImages\">Add Images</a>" );
+				submitBtn.before( fileInput );
+				fileInput.dropzone( getDropzoneConfig(ta, previewCtx, true) );
+
+			}
+
+			// Clear the previews frame when the post is submitted
+			submitBtn.on( "click", function ( ) {
+
+				previewCtx.empty();
+
+			});
+
+		// Do some additional magic with image links
+		// Useful when users drag an image from another browser window
+		// This feature can be toggled in the plugin config page.
+		if ( gdn.definition("processimageurls") === "1" ) {
+
+			ta.on( "drop", function ( e ) {
+
+				var originalEvt = e.originalEvent,
+					data = e.originalEvent.dataTransfer.getData('URL');
+
+				if ( data.length ) {
+
+					e.preventDefault();
+					e.stopPropagation();
+
+					if ( data.match(/([a-z\-_0-9\/\:\.]*\.(jpg|jpeg|png|gif))/i) ) {
+
+						insertAtCursor( ta[0], getLinkCode({link: data}) );
+
+					} else {
+
+						// Insert plaintext
+						insertAtCursor( ta[0], data );
+
+					}
+
+				}
+
+			});
+
+		}
+
+	});
+
+}( jQuery ));
